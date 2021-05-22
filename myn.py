bff5740b78d76cef03666e5ec56dcee327d06d49
Ferdie van Buren-Schele <ferdievbs@gmail.com>
2021-05-22 11:35:47 +0200

    Change to Postgre with error fix

--- myn.py
+++ myn.py
@@ -39,7 +39,7 @@ def load_user(user_id):
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
